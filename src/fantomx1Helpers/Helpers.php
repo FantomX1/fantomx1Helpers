df149d05cc2d38a814b1e147d546b23ef509b95e
frantisek.hallo <fantomx1@gmail.com>
2018-10-01 23:46:06 +0200

    few other might forgotten investigated helpers, later might come other found, by accident Yii2 began to dispone by very similar functionality, so others had similar idea

--- src/fantomx1Helpers/Helpers.php
+++ src/fantomx1Helpers/Helpers.php
@@ -12,6 +12,7 @@ class Helpers
 
     /**
      * indexes array by its specific recurring element value as a key
+     * @link http://php.net/manual/en/function.array-walk.php#119777
      * @param $array
      * @param $element
      * @return array
@@ -26,6 +27,65 @@ class Helpers
             }
         );
         return $arrayReindexed;
+    }
+
+
+    /**
+     * @link http://php.net/manual/en/function.array-merge-recursive.php#118727
+     * @param array $array1
+     * @param array $array2
+     * @return array
+     */
+    function array_merge_recursive_distinct ( array &$array1, array &$array2 )
+    {
+        static $level=0;
+        $merged = [];
+        if (!empty($array2["mergeWithParent"]) || $level == 0) {
+            $merged = $array1;
+        }
+
+        foreach ( $array2 as $key => &$value )
+        {
+            if (is_numeric($key)) {
+                $merged [] = $value;
+            } else {
+                $merged[$key] = $value;
+            }
+
+            if ( is_array ( $value ) && isset ( $array1 [$key] ) && is_array ( $array1 [$key] )
+            ) {
+                $level++;
+                $merged [$key] = array_merge_recursive_distinct($array1 [$key], $value);
+                $level--;
+            }
+        }
+        unset($merged["mergeWithParent"]);
+        return $merged;
+    }
+
+
+    /**
+     * @param array $array array to sort
+     * @param mixed $nThIndex by which index of multidimensional array at second level
+     * @param boolean $ascending
+     * @return array
+     */
+    public static function sortMultiDimArrayByNthElement($array, $nThIndex, $ascending = true)
+    {
+        usort(
+            $array,
+            function ($a, $b) use ($nThIndex) {
+                if ($a[$nThIndex] == $b[$nThIndex]) {
+                    return 0;
+                }
+                return ($a[$nThIndex] < $b[$nThIndex]) ? -1 : 1;
+            }
+        );
+        if (false === $ascending) {
+            // do descending
+            return array_reverse($array);
+        }
+        return $array;
     }
 
 }
